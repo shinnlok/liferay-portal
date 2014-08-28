shinnlok/liferay-portal

commit b82b41f1d0c7d094091e3ebbf5a8148ae55149c2

LPS-28959 Instantiate impls in registry and reference in utils

portal-impl/src/com/liferay/portlet/documentlibrary/util/DLProcessorRegistryImpl.java
@@ -19,15 +19,17 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
-import com.liferay.portal.kernel.util.InstancePool;
+import com.liferay.portal.kernel.util.InstanceFactory;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileVersion;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
+import com.liferay.portlet.documentlibrary.model.DLProcessorConstants;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Mika Koivisto
@@ -39,14 +41,7 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 			return;
 		}
 
-		for (String dlProcessorClassName : _DL_FILE_ENTRY_PROCESSORS) {
-			DLProcessor dlProcessor = (DLProcessor)InstancePool.get(
-				dlProcessorClassName);
-
-			dlProcessor.cleanUp(fileEntry);
-		}
-
-		for (DLProcessor dlProcessor : _dlProcessors) {
+		for (DLProcessor dlProcessor : _dlProcessors.values()) {
 			dlProcessor.cleanUp(fileEntry);
 		}
 	}
@@ -56,14 +51,7 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 			return;
 		}
 
-		for (String dlProcessorClassName : _DL_FILE_ENTRY_PROCESSORS) {
-			DLProcessor dlProcessor = (DLProcessor)InstancePool.get(
-				dlProcessorClassName);
-
-			dlProcessor.cleanUp(fileVersion);
-		}
-
-		for (DLProcessor dlProcessor : _dlProcessors) {
+		for (DLProcessor dlProcessor : _dlProcessors.values()) {
 			dlProcessor.cleanUp(fileVersion);
 		}
 	}
@@ -83,22 +71,16 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 			return;
 		}
 
-		for (String dlProcessorClassName : _DL_FILE_ENTRY_PROCESSORS) {
-			DLProcessor dlProcessor = (DLProcessor)InstancePool.get(
-				dlProcessorClassName);
-
+		for (DLProcessor dlProcessor : _dlProcessors.values()) {
 			if (dlProcessor.isSupported(latestFileVersion)) {
 				dlProcessor.exportGeneratedFiles(
 					portletDataContext, fileEntry, fileEntryElement);
 			}
 		}
+	}
 
-		for (DLProcessor dlProcessor : _dlProcessors) {
-			if (dlProcessor.isSupported(latestFileVersion)) {
-				dlProcessor.exportGeneratedFiles(
-					portletDataContext, fileEntry, fileEntryElement);
-			}
-		}
+	public DLProcessor getDLProcessor(String dlProcessorType) {
+		return _dlProcessors.get(dlProcessorType);
 	}
 
 	public void importGeneratedFiles(
@@ -116,18 +98,7 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 			return;
 		}
 
-		for (String dlProcessorClassName : _DL_FILE_ENTRY_PROCESSORS) {
-			DLProcessor dlProcessor = (DLProcessor)InstancePool.get(
-				dlProcessorClassName);
-
-			if (dlProcessor.isSupported(fileVersion)) {
-				dlProcessor.importGeneratedFiles(
-					portletDataContext, fileEntry, importedFileEntry,
-					fileEntryElement);
-			}
-		}
-
-		for (DLProcessor dlProcessor : _dlProcessors) {
+		for (DLProcessor dlProcessor : _dlProcessors.values()) {
 			if (dlProcessor.isSupported(fileVersion)) {
 				dlProcessor.importGeneratedFiles(
 					portletDataContext, fileEntry, importedFileEntry,
@@ -136,8 +107,21 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 		}
 	}
 
+	public void init() throws Exception {
+		ClassLoader classLoader = PortalClassLoaderUtil.getClassLoader();
+
+		for (String dlProcessorClassName : _DL_FILE_ENTRY_PROCESSORS) {
+			DLProcessor dlProcessor = (DLProcessor)InstanceFactory.newInstance(
+				classLoader, dlProcessorClassName);
+
+			register(dlProcessor);
+		}
+	}
+
 	public void register(DLProcessor dlProcessor) {
-		_dlProcessors.add(dlProcessor);
+		String type = _getType(dlProcessor);
+
+		_dlProcessors.put(type, dlProcessor);
 	}
 
 	public void trigger(FileEntry fileEntry, FileVersion fileVersion) {
@@ -155,24 +139,17 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 			return;
 		}
 
-		for (String dlProcessorClassName : _DL_FILE_ENTRY_PROCESSORS) {
-			DLProcessor dlProcessor = (DLProcessor)InstancePool.get(
-				dlProcessorClassName);
-
+		for (DLProcessor dlProcessor : _dlProcessors.values()) {
 			if (dlProcessor.isSupported(latestFileVersion)) {
 				dlProcessor.trigger(fileVersion, latestFileVersion);
-			}
-		}
-
-		for (DLProcessor dlProcessor : _dlProcessors) {
-			if (dlProcessor.isSupported(latestFileVersion)) {
-				dlProcessor.trigger(latestFileVersion, fileVersion);
 			}
 		}
 	}
 
 	public void unregister(DLProcessor dlProcessor) {
-		_dlProcessors.remove(dlProcessor);
+		String type = _getType(dlProcessor);
+
+		_dlProcessors.remove(type);
 	}
 
 	private FileVersion _getLatestFileVersion(FileEntry fileEntry) {
@@ -198,13 +175,33 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 		}
 	}
 
+	private String _getType(DLProcessor dlProcessor) {
+		if (dlProcessor instanceof AudioProcessor) {
+			return DLProcessorConstants.AUDIO_PROCESSOR;
+		}
+		else if (dlProcessor instanceof ImageProcessor) {
+			return DLProcessorConstants.IMAGE_PROCESSOR;
+		}
+		else if (dlProcessor instanceof PDFProcessor) {
+			return DLProcessorConstants.PDF_PROCESSOR;
+		}
+		else if (dlProcessor instanceof RawMetadataProcessor) {
+			return DLProcessorConstants.RAW_METADATA_PROCESSOR;
+		}
+		else if (dlProcessor instanceof VideoProcessor) {
+			return DLProcessorConstants.VIDEO_PROCESSOR;
+		}
+
+		return null;
+	}
+
 	private static final String[] _DL_FILE_ENTRY_PROCESSORS =
 		PropsUtil.getArray(PropsKeys.DL_FILE_ENTRY_PROCESSORS);
 
 	private static Log _log = LogFactoryUtil.getLog(
 		DLProcessorRegistryImpl.class);
 
-	private List<DLProcessor> _dlProcessors =
-		new CopyOnWriteArrayList<DLProcessor>();
+	private Map<String, DLProcessor> _dlProcessors =
+		new ConcurrentHashMap<String, DLProcessor>();
 
 }

portal-impl/src/META-INF/util-spring.xml
@@ -713,34 +713,9 @@
 			<bean class="com.liferay.portlet.documentlibrary.store.StoreProxyImpl" />
 		</constructor-arg>
 	</bean>
-	<bean id="com.liferay.portlet.documentlibrary.util.AudioProcessorUtil" class="com.liferay.portlet.documentlibrary.util.AudioProcessorUtil">
-		<property name="audioProcessor">
-			<bean class="com.liferay.portlet.documentlibrary.util.AudioProcessorImpl" factory-method="getInstance" />
-		</property>
-	</bean>
 	<bean id="com.liferay.portlet.documentlibrary.util.DLProcessorRegistryUtil" class="com.liferay.portlet.documentlibrary.util.DLProcessorRegistryUtil">
 		<property name="dLProcessorRegistry">
-			<bean class="com.liferay.portlet.documentlibrary.util.DLProcessorRegistryImpl" />
-		</property>
-	</bean>
-	<bean id="com.liferay.portlet.documentlibrary.util.ImageProcessorUtil" class="com.liferay.portlet.documentlibrary.util.ImageProcessorUtil">
-		<property name="imageProcessor">
-			<bean class="com.liferay.portlet.documentlibrary.util.ImageProcessorImpl" factory-method="getInstance" />
-		</property>
-	</bean>
-	<bean id="com.liferay.portlet.documentlibrary.util.PDFProcessorUtil" class="com.liferay.portlet.documentlibrary.util.PDFProcessorUtil">
-		<property name="pDFProcessor">
-			<bean class="com.liferay.portlet.documentlibrary.util.PDFProcessorImpl" factory-method="getInstance" />
-		</property>
-	</bean>
-	<bean id="com.liferay.portlet.documentlibrary.util.RawMetadataProcessorUtil" class="com.liferay.portlet.documentlibrary.util.RawMetadataProcessorUtil">
-		<property name="rawMetadataProcessor">
-			<bean class="com.liferay.portlet.documentlibrary.util.RawMetadataProcessorImpl" factory-method="getInstance" />
-		</property>
-	</bean>
-	<bean id="com.liferay.portlet.documentlibrary.util.VideoProcessorUtil" class="com.liferay.portlet.documentlibrary.util.VideoProcessorUtil">
-		<property name="videoProcessor">
-			<bean class="com.liferay.portlet.documentlibrary.util.VideoProcessorImpl" factory-method="getInstance" />
+			<bean class="com.liferay.portlet.documentlibrary.util.DLProcessorRegistryImpl" init-method="init" />
 		</property>
 	</bean>
 	<bean id="com.liferay.portlet.dynamicdatalists.util.DDLExporterFactory" class="com.liferay.portlet.dynamicdatalists.util.DDLExporterFactory">

portal-impl/src/com/liferay/portlet/documentlibrary/util/AudioProcessorImpl.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.process.ProcessExecutor;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.kernel.util.SetUtil;
@@ -63,41 +62,68 @@ import org.apache.commons.lang.time.StopWatch;
 public class AudioProcessorImpl
 	extends DLPreviewableProcessor implements AudioProcessor {
 
-	public static AudioProcessorImpl getInstance() {
-		return _instance;
+	public AudioProcessorImpl() {
+		boolean valid = true;
+
+		if ((_PREVIEW_TYPES.length == 0) || (_PREVIEW_TYPES.length > 2)) {
+			valid = false;
+		}
+		else {
+			for (String previewType : _PREVIEW_TYPES) {
+				if (!previewType.equals("mp3") && !previewType.equals("ogg")) {
+					valid = false;
+
+					break;
+				}
+			}
+		}
+
+		if (!valid && _log.isWarnEnabled()) {
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("Liferay is incorrectly configured to generate video ");
+			sb.append("previews using video containers other than MP3 or ");
+			sb.append("OGG. Please change the property ");
+			sb.append(PropsKeys.DL_FILE_ENTRY_PREVIEW_AUDIO_CONTAINERS);
+			sb.append(" in portal-ext.properties.");
+
+			_log.warn(sb.toString());
+		}
+
+		FileUtil.mkdirs(PREVIEW_TMP_PATH);
 	}
 
 	public void generateAudio(
 			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
 		throws Exception {
 
-		_instance._generateAudio(sourceFileVersion, destinationFileVersion);
+		_generateAudio(sourceFileVersion, destinationFileVersion);
 	}
 
 	public Set<String> getAudioMimeTypes() {
-		return _instance._audioMimeTypes;
+		return _audioMimeTypes;
 	}
 
 	public InputStream getPreviewAsStream(FileVersion fileVersion, String type)
 		throws Exception {
 
-		return _instance.doGetPreviewAsStream(fileVersion, type);
+		return doGetPreviewAsStream(fileVersion, type);
 	}
 
 	public long getPreviewFileSize(FileVersion fileVersion, String type)
 		throws Exception {
 
-		return _instance.doGetPreviewFileSize(fileVersion, type);
+		return doGetPreviewFileSize(fileVersion, type);
 	}
 
 	public boolean hasAudio(FileVersion fileVersion) {
 		boolean hasAudio = false;
 
 		try {
-			hasAudio = _instance._hasAudio(fileVersion);
+			hasAudio = _hasAudio(fileVersion);
 
-			if (!hasAudio && _instance.isSupported(fileVersion)) {
-				_instance._queueGeneration(null, fileVersion);
+			if (!hasAudio && isSupported(fileVersion)) {
+				_queueGeneration(null, fileVersion);
 			}
 		}
 		catch (Exception e) {
@@ -108,11 +134,11 @@ public class AudioProcessorImpl
 	}
 
 	public boolean isAudioSupported(FileVersion fileVersion) {
-		return _instance.isSupported(fileVersion);
+		return isSupported(fileVersion);
 	}
 
 	public boolean isAudioSupported(String mimeType) {
-		return _instance.isSupported(mimeType);
+		return isSupported(mimeType);
 	}
 
 	public boolean isSupported(String mimeType) {
@@ -134,7 +160,7 @@ public class AudioProcessorImpl
 	public void trigger(
 		FileVersion sourceFileVersion, FileVersion destinationFileVersion) {
 
-		_instance._queueGeneration(sourceFileVersion, destinationFileVersion);
+		_queueGeneration(sourceFileVersion, destinationFileVersion);
 	}
 
 	@Override
@@ -213,37 +239,6 @@ public class AudioProcessorImpl
 					fileEntryElement, "audio", previewType);
 			}
 		}
-	}
-
-	private AudioProcessorImpl() {
-		boolean valid = true;
-
-		if ((_PREVIEW_TYPES.length == 0) || (_PREVIEW_TYPES.length > 2)) {
-			valid = false;
-		}
-		else {
-			for (String previewType : _PREVIEW_TYPES) {
-				if (!previewType.equals("mp3") && !previewType.equals("ogg")) {
-					valid = false;
-
-					break;
-				}
-			}
-		}
-
-		if (!valid && _log.isWarnEnabled()) {
-			StringBundler sb = new StringBundler(5);
-
-			sb.append("Liferay is incorrectly configured to generate video ");
-			sb.append("previews using video containers other than MP3 or ");
-			sb.append("OGG. Please change the property ");
-			sb.append(PropsKeys.DL_FILE_ENTRY_PREVIEW_AUDIO_CONTAINERS);
-			sb.append(" in portal-ext.properties.");
-
-			_log.warn(sb.toString());
-		}
-
-		FileUtil.mkdirs(PREVIEW_TMP_PATH);
 	}
 
 	private void _generateAudio(
@@ -430,12 +425,6 @@ public class AudioProcessorImpl
 		PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_CONTAINERS;
 
 	private static Log _log = LogFactoryUtil.getLog(AudioProcessor.class);
-
-	private static AudioProcessorImpl _instance = new AudioProcessorImpl();
-
-	static {
-		InstancePool.put(AudioProcessorImpl.class.getName(), _instance);
-	}
 
 	private Set<String> _audioMimeTypes = SetUtil.fromArray(
 		PropsValues.DL_FILE_ENTRY_PREVIEW_AUDIO_MIME_TYPES);

portal-impl/src/com/liferay/portlet/documentlibrary/util/ImageProcessorImpl.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.util.FileUtil;
-import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -53,8 +52,7 @@ import java.util.Vector;
 public class ImageProcessorImpl
 	extends DLPreviewableProcessor implements ImageProcessor {
 
-	public static ImageProcessorImpl getInstance() {
-		return _instance;
+	public ImageProcessorImpl() {
 	}
 
 	@Override
@@ -64,7 +62,7 @@ public class ImageProcessorImpl
 
 	@Override
 	public void cleanUp(FileVersion fileVersion) {
-		String type = _instance.getThumbnailType(fileVersion);
+		String type = getThumbnailType(fileVersion);
 
 		deleteFiles(fileVersion, type);
 	}
@@ -73,11 +71,11 @@ public class ImageProcessorImpl
 			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
 		throws Exception {
 
-		_instance._generateImages(sourceFileVersion, destinationFileVersion);
+		_generateImages(sourceFileVersion, destinationFileVersion);
 	}
 
 	public Set<String> getImageMimeTypes() {
-		return _instance._imageMimeTypes;
+		return _imageMimeTypes;
 	}
 
 	public InputStream getPreviewAsStream(FileVersion fileVersion)
@@ -86,7 +84,7 @@ public class ImageProcessorImpl
 		if (_previewGenerationRequired(fileVersion)) {
 			String type = getPreviewType(fileVersion);
 
-			return _instance.doGetPreviewAsStream(fileVersion, type);
+			return doGetPreviewAsStream(fileVersion, type);
 		}
 
 		return fileVersion.getContentStream(false);
@@ -96,7 +94,7 @@ public class ImageProcessorImpl
 		if (_previewGenerationRequired(fileVersion)) {
 			String type = getPreviewType(fileVersion);
 
-			return _instance.doGetPreviewFileSize(fileVersion, type);
+			return doGetPreviewFileSize(fileVersion, type);
 		}
 
 		return fileVersion.getSize();
@@ -110,13 +108,13 @@ public class ImageProcessorImpl
 	public InputStream getThumbnailAsStream(FileVersion fileVersion, int index)
 		throws Exception {
 
-		return _instance.doGetThumbnailAsStream(fileVersion, index);
+		return doGetThumbnailAsStream(fileVersion, index);
 	}
 
 	public long getThumbnailFileSize(FileVersion fileVersion, int index)
 		throws Exception {
 
-		return _instance.doGetThumbnailFileSize(fileVersion, index);
+		return doGetThumbnailFileSize(fileVersion, index);
 	}
 
 	@Override
@@ -134,14 +132,13 @@ public class ImageProcessorImpl
 		boolean hasImages = false;
 
 		try {
-			if (_instance._hasPreview(fileVersion) &&
-				hasThumbnails(fileVersion)) {
+			if (_hasPreview(fileVersion) && hasThumbnails(fileVersion)) {
 
 				hasImages = true;
 			}
 
-			if (!hasImages && _instance.isSupported(fileVersion)) {
-				_instance._queueGeneration(null, fileVersion);
+			if (!hasImages && isSupported(fileVersion)) {
+				_queueGeneration(null, fileVersion);
 			}
 		}
 		catch (Exception e) {
@@ -152,11 +149,11 @@ public class ImageProcessorImpl
 	}
 
 	public boolean isImageSupported(FileVersion fileVersion) {
-		return _instance.isSupported(fileVersion);
+		return isSupported(fileVersion);
 	}
 
 	public boolean isImageSupported(String mimeType) {
-		return _instance.isSupported(mimeType);
+		return isSupported(mimeType);
 	}
 
 	public boolean isSupported(String mimeType) {
@@ -173,7 +170,7 @@ public class ImageProcessorImpl
 			String type)
 		throws Exception {
 
-		_instance._storeThumbnail(
+		_storeThumbnail(
 			companyId, groupId, fileEntryId, fileVersionId, custom1ImageId,
 			custom2ImageId, is, type);
 	}
@@ -181,7 +178,7 @@ public class ImageProcessorImpl
 	public void trigger(
 		FileVersion sourceFileVersion, FileVersion destinationFileVersion) {
 
-		_instance._queueGeneration(sourceFileVersion, destinationFileVersion);
+		_queueGeneration(sourceFileVersion, destinationFileVersion);
 	}
 
 	@Override
@@ -234,9 +231,6 @@ public class ImageProcessorImpl
 		exportPreview(
 			portletDataContext, fileEntry, fileEntryElement, "image",
 			getPreviewType(fileVersion));
-	}
-
-	private ImageProcessorImpl() {
 	}
 
 	private void _generateImages(
@@ -433,12 +427,6 @@ public class ImageProcessorImpl
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(ImageProcessorImpl.class);
-
-	private static ImageProcessorImpl _instance = new ImageProcessorImpl();
-
-	static {
-		InstancePool.put(ImageProcessorImpl.class.getName(), _instance);
-	}
 
 	private List<Long> _fileVersionIds = new Vector<Long>();
 	private Set<String> _imageMimeTypes = SetUtil.fromArray(

portal-impl/src/com/liferay/portlet/documentlibrary/util/PDFProcessorImpl.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -66,29 +65,34 @@ import org.im4java.core.IMOperation;
 public class PDFProcessorImpl
 	extends DLPreviewableProcessor implements PDFProcessor {
 
-	public static PDFProcessorImpl getInstance() {
-		return _instance;
+	public PDFProcessorImpl() {
+		try {
+			FileUtil.mkdirs(PREVIEW_TMP_PATH);
+			FileUtil.mkdirs(THUMBNAIL_TMP_PATH);
+
+			ImageMagickUtil.reset();
+		}
+		catch (Exception e) {
+			_log.warn(e, e);
+		}
 	}
 
 	public void generateImages(
 			FileVersion sourceFileVersion, FileVersion destinationFileVersion)
 		throws Exception {
 
-		Initializer._initializedInstance._generateImages(
-			sourceFileVersion, destinationFileVersion);
+		_generateImages(sourceFileVersion, destinationFileVersion);
 	}
 
 	public InputStream getPreviewAsStream(FileVersion fileVersion, int index)
 		throws Exception {
 
-		return Initializer._initializedInstance.doGetPreviewAsStream(
-			fileVersion, index, PREVIEW_TYPE);
+		return doGetPreviewAsStream(fileVersion, index, PREVIEW_TYPE);
 	}
 
 	public int getPreviewFileCount(FileVersion fileVersion) {
 		try {
-			return Initializer._initializedInstance.doGetPreviewFileCount(
-				fileVersion);
+			return doGetPreviewFileCount(fileVersion);
 		}
 		catch (Exception e) {
 			_log.error(e, e);
@@ -100,8 +104,7 @@ public class PDFProcessorImpl
 	public long getPreviewFileSize(FileVersion fileVersion, int index)
 		throws Exception {
 
-		return Initializer._initializedInstance.doGetPreviewFileSize(
-			fileVersion, index);
+		return doGetPreviewFileSize(fileVersion, index);
 	}
 
 	public InputStream getThumbnailAsStream(FileVersion fileVersion, int index)
@@ -123,8 +126,7 @@ public class PDFProcessorImpl
 			hasImages = _hasImages(fileVersion);
 
 			if (!hasImages && isSupported(fileVersion)) {
-				Initializer._initializedInstance._queueGeneration(
-					null, fileVersion);
+				_queueGeneration(null, fileVersion);
 			}
 		}
 		catch (Exception e) {
@@ -135,11 +137,11 @@ public class PDFProcessorImpl
 	}
 
 	public boolean isDocumentSupported(FileVersion fileVersion) {
-		return Initializer._initializedInstance.isSupported(fileVersion);
+		return isSupported(fileVersion);
 	}
 
 	public boolean isDocumentSupported(String mimeType) {
-		return Initializer._initializedInstance.isSupported(mimeType);
+		return isSupported(mimeType);
 	}
 
 	public boolean isSupported(String mimeType) {
@@ -174,8 +176,7 @@ public class PDFProcessorImpl
 	public void trigger(
 		FileVersion sourceFileVersion, FileVersion destinationFileVersion) {
 
-		Initializer._initializedInstance._queueGeneration(
-			sourceFileVersion, destinationFileVersion);
+		_queueGeneration(sourceFileVersion, destinationFileVersion);
 	}
 
 	@Override
@@ -248,8 +249,7 @@ public class PDFProcessorImpl
 		}
 
 		if (!portletDataContext.isPerformDirectBinaryImport()) {
-			int previewFileCount = PDFProcessorUtil.getPreviewFileCount(
-				fileVersion);
+			int previewFileCount = getPreviewFileCount(fileVersion);
 
 			fileEntryElement.addAttribute(
 				"bin-path-pdf-preview-count", String.valueOf(previewFileCount));
@@ -299,21 +299,6 @@ public class PDFProcessorImpl
 				portletDataContext, fileEntry, importedFileEntry,
 				fileEntryElement, "pdf", PREVIEW_TYPE, i);
 		}
-	}
-
-	protected void initialize() {
-		try {
-			FileUtil.mkdirs(PREVIEW_TMP_PATH);
-			FileUtil.mkdirs(THUMBNAIL_TMP_PATH);
-
-			ImageMagickUtil.reset();
-		}
-		catch (Exception e) {
-			_log.warn(e, e);
-		}
-	}
-
-	private PDFProcessorImpl() {
 	}
 
 	private void _generateImages(FileVersion fileVersion, File file)
@@ -730,24 +715,6 @@ public class PDFProcessorImpl
 
 	private static Log _log = LogFactoryUtil.getLog(PDFProcessorImpl.class);
 
-	private static PDFProcessorImpl _instance = new PDFProcessorImpl();
-
-	static {
-		InstancePool.put(PDFProcessorImpl.class.getName(), _instance);
-	}
-
 	private List<Long> _fileVersionIds = new Vector<Long>();
-
-	private static class Initializer {
-
-		private static PDFProcessorImpl _initializedInstance;
-
-		static {
-			_instance.initialize();
-
-			_initializedInstance = _instance;
-		}
-
-	}
 
 }

portal-impl/src/com/liferay/portlet/documentlibrary/util/RawMetadataProcessorImpl.java
@@ -28,7 +28,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
-import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
@@ -55,10 +54,9 @@ import java.util.Map;
  * @author Miguel Pastor
  */
 public class RawMetadataProcessorImpl
-	extends BaseDLProcessorImpl implements RawMetadataProcessor {
+	implements DLProcessor, RawMetadataProcessor {
 
-	public static RawMetadataProcessorImpl getInstance() {
-		return _instance;
+	public RawMetadataProcessorImpl() {
 	}
 
 	public void cleanUp(FileEntry fileEntry) {
@@ -82,16 +80,12 @@ public class RawMetadataProcessorImpl
 	public void generateMetadata(FileVersion fileVersion)
 		throws SystemException {
 
-		if (!isEnabled()) {
-			return;
-		}
-
 		long fileEntryMetadataCount =
 			DLFileEntryMetadataLocalServiceUtil.getFileEntryMetadataCount(
 				fileVersion.getFileEntryId(), fileVersion.getFileVersionId());
 
 		if (fileEntryMetadataCount == 0) {
-			_instance.trigger(fileVersion);
+			trigger(fileVersion);
 		}
 	}
 
@@ -113,10 +107,6 @@ public class RawMetadataProcessorImpl
 
 	public void saveMetadata(FileVersion fileVersion)
 		throws PortalException, SystemException {
-
-		if (!isEnabled()) {
-			return;
-		}
 
 		Map<String, Fields> rawMetadataMap = null;
 
@@ -184,10 +174,6 @@ public class RawMetadataProcessorImpl
 	public void trigger(
 		FileVersion sourceFileVersion, FileVersion destinationFileVersion) {
 
-		if (!isEnabled()) {
-			return;
-		}
-
 		if (PropsValues.DL_FILE_ENTRY_PROCESSORS_TRIGGER_SYNCHRONOUSLY) {
 			try {
 				MessageBusUtil.sendSynchronousMessage(
@@ -207,17 +193,7 @@ public class RawMetadataProcessorImpl
 		}
 	}
 
-	private RawMetadataProcessorImpl() {
-	}
-
 	private static Log _log = LogFactoryUtil.getLog(
 		RawMetadataProcessorImpl.class);
-
-	private static RawMetadataProcessorImpl _instance =
-		new RawMetadataProcessorImpl();
-
-	static {
-		InstancePool.put(RawMetadataProcessorImpl.class.getName(), _instance);
-	}
 
 }

commit f97bfd7f02d4d7e9a70c4c9dea5aea062ada628b

LPS-49670 DLProcessorRegistryImpl cannot register custom DLProcessor from a hook and causes NPE. Caused by b82b41f1d0c7d094091e3ebbf5a8148ae55149c2 - LPS-28959

portal-impl/src/com/liferay/portlet/documentlibrary/util/DLProcessorRegistryImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portlet.documentlibrary.util;
 
+import com.liferay.portal.kernel.bean.ClassLoaderBeanHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -23,6 +24,7 @@ import com.liferay.portal.kernel.security.pacl.DoPrivileged;
 import com.liferay.portal.kernel.util.InstanceFactory;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileVersion;
 import com.liferay.portal.util.ClassLoaderUtil;
@@ -30,6 +32,8 @@ import com.liferay.portal.util.PrefsPropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLProcessorConstants;
+
+import java.lang.reflect.InvocationHandler;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -163,7 +167,9 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 	public void register(DLProcessor dlProcessor) {
 		String type = _getType(dlProcessor);
 
-		_dlProcessors.put(type, dlProcessor);
+		if (type != null) {
+			_dlProcessors.put(type, dlProcessor);
+		}
 	}
 
 	@Override
@@ -230,6 +236,22 @@ public class DLProcessorRegistryImpl implements DLProcessorRegistry {
 	}
 
 	private String _getType(DLProcessor dlProcessor) {
+		if (ProxyUtil.isProxyClass(dlProcessor.getClass())) {
+			InvocationHandler invocationHandler =
+				ProxyUtil.getInvocationHandler(dlProcessor);
+
+			if (invocationHandler instanceof ClassLoaderBeanHandler) {
+				ClassLoaderBeanHandler classLoaderBeanHandler =
+					(ClassLoaderBeanHandler)invocationHandler;
+
+				Object bean = classLoaderBeanHandler.getBean();
+
+				if (bean instanceof DLProcessor) {
+					dlProcessor = (DLProcessor)bean;
+				}
+			}
+		}
+
 		if (dlProcessor instanceof AudioProcessor) {
 			return DLProcessorConstants.AUDIO_PROCESSOR;
 		}