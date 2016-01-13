ba76500414cf39688bb45745fc2bb56cc101cb1f
Shinn Lok <shinn.lok@liferay.com>
2016-01-13 11:50:01 -0800

    Move batch call

--- modules/util/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/handler/GetSyncDLObjectUpdateHandler.java
+++ modules/util/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/handler/GetSyncDLObjectUpdateHandler.java
@@ -17,6 +17,8 @@ package com.liferay.sync.engine.documentlibrary.handler;
 import com.liferay.sync.engine.documentlibrary.event.Event;
 import com.liferay.sync.engine.documentlibrary.event.GetSyncContextEvent;
 import com.liferay.sync.engine.documentlibrary.model.SyncDLObjectUpdate;
+import com.liferay.sync.engine.documentlibrary.util.BatchDownloadEvent;
+import com.liferay.sync.engine.documentlibrary.util.BatchEventManager;
 import com.liferay.sync.engine.documentlibrary.util.FileEventUtil;
 import com.liferay.sync.engine.documentlibrary.util.comparator.SyncFileSizeComparator;
 import com.liferay.sync.engine.filesystem.Watcher;
@@ -237,6 +239,11 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 					GetterUtil.getBoolean(
 						getParameterValue("retrieveFromCache")));
 			}
+
+			BatchDownloadEvent batchDownloadEvent =
+				BatchEventManager.getBatchDownloadEvent(getSyncAccountId());
+
+			batchDownloadEvent.fireBatchEvent();
 		}
 	}
 
