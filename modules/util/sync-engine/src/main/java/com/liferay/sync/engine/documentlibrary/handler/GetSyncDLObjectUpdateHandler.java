37e1db669f1f156d239ea95c55933ea836e5ec3e
Shinn Lok <shinn.lok@liferay.com>
2016-01-20 14:16:40 -0800

    SYNC-1526 Apply

--- modules/util/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/handler/GetSyncDLObjectUpdateHandler.java
+++ modules/util/sync-engine/src/main/java/com/liferay/sync/engine/documentlibrary/handler/GetSyncDLObjectUpdateHandler.java
@@ -177,12 +177,14 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 
 		SyncSite syncSite = (SyncSite)getParameterValue("syncSite");
 
-		syncSite = SyncSiteService.fetchSyncSite(
-			syncSite.getGroupId(), syncSite.getSyncAccountId());
+		if (syncSite != null) {
+			syncSite = SyncSiteService.fetchSyncSite(
+				syncSite.getGroupId(), syncSite.getSyncAccountId());
 
-		syncSite.setState(SyncSite.STATE_SYNCED);
+			syncSite.setState(SyncSite.STATE_SYNCED);
 
-		SyncSiteService.update(syncSite);
+			SyncSiteService.update(syncSite);
+		}
 	}
 
 	@Override
@@ -252,8 +254,10 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 
 		Path filePath = Paths.get(filePathName);
 
-		if (Files.exists(filePath) &&
-			(syncFile.isFolder() || !FileUtil.isModified(syncFile, filePath))) {
+		if (isParentUnsynced(syncFile) ||
+			(Files.exists(filePath) &&
+			 (syncFile.isFolder() ||
+			  !FileUtil.isModified(syncFile, filePath)))) {
 
 			return;
 		}
@@ -458,6 +462,26 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 		return FileUtil.isIgnoredFilePath(FileUtil.getFilePath(filePathName));
 	}
 
+	protected boolean isParentUnsynced(SyncFile syncFile) {
+		Path filePath = Paths.get(syncFile.getFilePathName());
+
+		if (FileUtil.isUnsynced(filePath.getParent())) {
+			if (syncFile.isFolder()) {
+				syncFile.setFilePathName(filePath.toString());
+				syncFile.setModifiedTime(0);
+				syncFile.setState(SyncFile.STATE_UNSYNCED);
+				syncFile.setUiEvent(SyncFile.UI_EVENT_NONE);
+				syncFile.setSyncAccountId(getSyncAccountId());
+
+				SyncFileService.update(syncFile);
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
 	@Override
 	protected void logResponse(String response) {
 		try {
@@ -489,13 +513,28 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 			String targetFilePathName)
 		throws Exception {
 
+		if (isParentUnsynced(targetSyncFile)) {
+			deleteFile(sourceSyncFile, false);
+
+			return;
+		}
+
 		Path sourceFilePath = Paths.get(sourceSyncFile.getFilePathName());
 		Path targetFilePath = Paths.get(targetFilePathName);
 
 		sourceSyncFile = SyncFileService.updateSyncFile(
 			targetFilePath, targetSyncFile.getParentFolderId(), sourceSyncFile);
 
-		if (Files.exists(sourceFilePath)) {
+		if (sourceSyncFile.isUnsynced()) {
+			List<SyncFile> syncFiles = new ArrayList<>();
+
+			syncFiles.add(sourceSyncFile);
+
+			SyncFileService.resyncFolders(syncFiles);
+
+			return;
+		}
+		else if (Files.exists(sourceFilePath)) {
 			FileUtil.moveFile(sourceFilePath, targetFilePath);
 
 			sourceSyncFile.setState(SyncFile.STATE_SYNCED);
@@ -523,7 +562,7 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 		List<SyncFile> dependentSyncFiles = _dependentSyncFilesMap.remove(
 			getSyncAccountId() + "#" + syncFile.getTypePK());
 
-		if (dependentSyncFiles == null) {
+		if (syncFile.isUnsynced() || (dependentSyncFiles == null)) {
 			return;
 		}
 
@@ -611,6 +650,9 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 
 					return;
 				}
+				else if (isParentUnsynced(targetSyncFile)) {
+					return;
+				}
 
 				targetSyncFile.setLocalExtraSetting("restoreEvent", true);
 
@@ -695,9 +737,13 @@ public class GetSyncDLObjectUpdateHandler extends BaseSyncDLObjectHandler {
 
 		SyncFileService.update(sourceSyncFile);
 
+		if (isParentUnsynced(sourceSyncFile)) {
+			return;
+		}
+
 		Path filePath = Paths.get(targetSyncFile.getFilePathName());
 
-		if (filePathChanged && !Files.exists(filePath)) {
+		if (!Files.exists(filePath)) {
 			if (targetSyncFile.isFolder()) {
 				Path targetFilePath = Paths.get(filePathName);
 
