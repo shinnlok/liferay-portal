50b2a651d188aebb0077bfbb4bf15546dccac6f8
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-01-10 17:27:20 -0800

    LRAC-4818 clean up messages after disconnecting

--- modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/AnalyticsMessageSenderClientImpl.java
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/AnalyticsMessageSenderClientImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.analytics.message.sender.internal;
 
 import com.liferay.analytics.message.sender.client.AnalyticsMessageSenderClient;
+import com.liferay.analytics.message.storage.service.AnalyticsMessageLocalService;
 import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
 import com.liferay.analytics.settings.configuration.AnalyticsConfigurationTracker;
 import com.liferay.analytics.settings.security.constants.AnalyticsSecurityConstants;
@@ -189,6 +190,9 @@ public class AnalyticsMessageSenderClientImpl
 
 			if (message.equals("INVALID_TOKEN")) {
 				_disconnectDataSource(companyId);
+
+				_analyticsMessageLocalService.deleteAnalyticsMessages(
+					companyId);
 			}
 
 			return closeableHttpResponse;
@@ -200,6 +204,9 @@ public class AnalyticsMessageSenderClientImpl
 
 	@Reference
 	private AnalyticsConfigurationTracker _analyticsConfigurationTracker;
+
+	@Reference
+	private AnalyticsMessageLocalService _analyticsMessageLocalService;
 
 	@Reference
 	private CompanyService _companyService;
